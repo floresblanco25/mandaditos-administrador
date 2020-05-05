6e37092cda365f79044a77290872b05b28a7eb72
floresblanco25@gmail.com <floresblanco25@gmail.com>
2020-05-05 15:15:29 -0600

    updated

--- app/src/main/java/com/mandaditos/administrador/mAdapter.java
+++ app/src/main/java/com/mandaditos/administrador/mAdapter.java
@@ -8,6 +8,7 @@ import android.widget.*;
 import com.google.firebase.database.*;
 import com.mandaditos.administrador.models.*;
 import java.util.*;
+import android.app.*;
 
 public class mAdapter extends RecyclerView.Adapter<mViewHolder>
 {
@@ -37,19 +38,86 @@ public class mAdapter extends RecyclerView.Adapter<mViewHolder>
 		holder.EstadoDeOrdenEd.setText(mDataList.get(position).getEstadoDeOrden());
 		holder.NumeroDeOrdenEd.setText(mDataList.get(position).getNumeroDeOrden());
 		holder.NumeroDeOrdenEd.setEnabled(false);
+		holder.PartidaEd.setEnabled(false);
+		holder.DestinoEd.setEnabled(false);
+		holder.DistanciaEd.setEnabled(false);
+		holder.FechaEtaEd.setEnabled(false);
+		holder.DondeRecogerDineroEd.setEnabled(false);
+		holder.CostoEd.setEnabled(false);
+		holder.EstadoDeOrdenEd.setEnabled(false);
+		holder.save.setEnabled(false);
+		
+		
+		
 		
 		holder.save.setOnClickListener(new OnClickListener(){
 
 				@Override
 				public void onClick(View p1)
 				{
-					String ordrStat = holder.EstadoDeOrdenEd.getText().toString();
-					DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("Ordenes").child(mDataList.get(position).getNumeroDeOrden());
-					mDatabase.child("estadoDeOrden").setValue(ordrStat);
+					final AlertDialog dialog = new AlertDialog.Builder(holder.context).create();
+					dialog.setTitle("Alerta!");
+					dialog.setMessage("Asegurate que los datos sean correctos");
+					dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancelar", new DialogInterface.OnClickListener(){
+
+							@Override
+							public void onClick(DialogInterface p1, int p2)
+							{
+								dialog.dismiss();
+							}
+						});
+					dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Continuar", new DialogInterface.OnClickListener(){
+
+							@Override
+							public void onClick(DialogInterface p1, int p2)
+							{
+								String ordrStat = holder.EstadoDeOrdenEd.getText().toString();
+								String partida = holder.PartidaEd.getText().toString();
+								String destino = holder.DestinoEd.getText().toString();
+								String distancia = holder.DistanciaEd.getText().toString();
+								String recogerdinero = holder.DondeRecogerDineroEd.getText().toString();
+								String costo = holder.CostoEd.getText().toString();
+								DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("Ordenes").child(mDataList.get(position).getNumeroDeOrden());
+								mDatabase.child("estadoDeOrden").setValue(ordrStat);
+								mDatabase.child("partida").setValue(partida);
+								mDatabase.child("destino").setValue(destino);
+								mDatabase.child("distancia").setValue(distancia);
+								mDatabase.child("recogerDineroEn").setValue(recogerdinero);
+								mDatabase.child("costo").setValue(costo);
+								holder.PartidaEd.setEnabled(false);
+								holder.DestinoEd.setEnabled(false);
+								holder.DistanciaEd.setEnabled(false);
+								holder.FechaEtaEd.setEnabled(false);
+								holder.DondeRecogerDineroEd.setEnabled(false);
+								holder.CostoEd.setEnabled(false);
+								holder.EstadoDeOrdenEd.setEnabled(false);
+								holder.save.setEnabled(false);
+							}
+						});
+						dialog.show();
+
+				}
+
+			});
+		holder.edit.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					holder.PartidaEd.setEnabled(true);
+					holder.DestinoEd.setEnabled(true);
+					holder.DistanciaEd.setEnabled(true);
+					holder.FechaEtaEd.setEnabled(true);
+					holder.DondeRecogerDineroEd.setEnabled(true);
+					holder.CostoEd.setEnabled(true);
+					holder.EstadoDeOrdenEd.setEnabled(true);
+					holder.save.setEnabled(true);
 				}
 			});
+			
     }
-
+	
+	
     @Override
     public int getItemCount() {
         return mDataList.size();
@@ -59,8 +127,8 @@ public class mAdapter extends RecyclerView.Adapter<mViewHolder>
 class mViewHolder extends RecyclerView.ViewHolder {
 
     EditText NumeroDeOrdenEd,PartidaEd,DestinoEd,DistanciaEd,FechaEtaEd,DondeRecogerDineroEd,CostoEd,EstadoDeOrdenEd;
-	Button save;
-	private Context context;
+	Button save,edit;
+	Context context;
 
     mViewHolder(View v) {
         super(v);
@@ -74,9 +142,12 @@ class mViewHolder extends RecyclerView.ViewHolder {
 		CostoEd = v.findViewById(R.id.dashboardTotalCost);
 		EstadoDeOrdenEd = v.findViewById(R.id.dashboardOrderStatus);
 		save = v.findViewById(R.id.orderrowButtonGuardar);
+		edit = v.findViewById(R.id.orderrowButtonEditar);
 		context = v.getContext();
 
     }
+	
+	
 
 
 
